696509a4cfac79250225d17e764691e750df5a45
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-01-17 22:30:47 -0600

    Solution from problem 5755 added

---
+++ nivel_basico/5755.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int x1, y1, x2, y2, x3, y3;
+    scanf("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &x3, &y3);
+
+    int area = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+    if (area)
+        printf("%c", 'N');
+    else
+        printf("%c", 'S');
+
+    return 0;
+}
